07eee9653873283c8ccdf4fc8b6d27ef589a3e6b
hcpl <hcpl.prog@gmail.com>
2016-11-01 23:14:15 +0200

    main: remove redundant flags

--- xkbgroup/__main__.py
+++ xkbgroup/__main__.py
@@ -43,17 +43,11 @@ def create_argument_parser():
     parser_get = subparsers.add_parser("get")
     parser_get.set_defaults(func=xkb_get)
     parser_get.add_argument("attribute", choices=GET_CHOICES)
-    get_all_or_current = parser_get.add_mutually_exclusive_group()
-    get_all_or_current.add_argument("-a", "--all", dest="get_all", action="store_true")
-    get_all_or_current.add_argument("-c", "--current", dest="get_all", action="store_false")
 
     parser_set = subparsers.add_parser("set")
     parser_set.set_defaults(func=xkb_set)
     parser_set.add_argument("attribute", choices=SET_CHOICES)
     parser_set.add_argument("value")
-    set_all_or_current = parser_set.add_mutually_exclusive_group()
-    set_all_or_current.add_argument("-a", "--all", dest="get_all", action="store_true")
-    set_all_or_current.add_argument("-c", "--current", dest="get_all", action="store_false")
 
     return parser
 
